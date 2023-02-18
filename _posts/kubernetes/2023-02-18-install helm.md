44bf272502b653589f718e9d62d5a81512a4b671
yoonjk <nexweb1@gmail.com>
2023-02-18 12:11:19 +0900

    Add Helm CLI

---
+++ _posts/kubernetes/2023-02-18-install helm.md
@@ -0,0 +1,15 @@
+---
+title:  Helm 설치
+categories:
+  - kubernetes
+tags: 
+  - helm
+---
+
+## Helm CLI를  설치
+Helm Chart를 설치하기 위해 Helm CLI를 설치합니다.  
+```bash
+ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+```
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -244,4 +244,10 @@ var store = [{
         "tags": ["jenkins","master","slave"],
         "url": "/devops/jenkins-controller-and-agent/",
         "teaser": null
+      },{
+        "title": "Helm 설치",
+        "excerpt":"Helm CLI를  설치  Helm Chart를 설치하기 위해 Helm CLI를 설치합니다.   curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 chmod 700 get_helm.sh ./get_helm.sh  ","categories": ["kubernetes"],
+        "tags": ["helm"],
+        "url": "/kubernetes/install-helm/",
+        "teaser": null
       }]
