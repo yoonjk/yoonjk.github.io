68df1f2cdea66d859f5fc6f1e1f6530e6ef591c3
yoonjk <nexweb1@gmail.com>
2023-04-08 17:04:06 +0900

    add redis sets

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -455,6 +455,12 @@ var store = [{
         "url": "/cache/list/",
         "teaser": null
       },{
+        "title": "Getting Start Redis - Set",
+        "excerpt":"Sets Redis 집합은 순서가 지정되지 않은 문자열 컬렉션입니다. SADD 명령은 세트에 새 요소를 추가합니다. 주어진 요소가 이미 존재하는지 테스트, 여러 집합 간의 교차, 합집합 또는 차이 수행 등과 같은 집합에 대해 여러 가지 다른 작업을 수행 할 수도 있습니다. &gt; sadd myset 1 2 3 (integer) 3 &gt; smembers myset...","categories": ["cache"],
+        "tags": ["redis","jupyter"],
+        "url": "/cache/set/",
+        "teaser": null
+      },{
         "title": "Getting Start Redis - String",
         "excerpt":"Strings Redis 문자열 유형은 Redis 키와 연결할 수 있는 가장 간단한 값 유형입니다. Memcached의 유일한 데이터 유형이므로 신규 사용자가 Redis에서 사용하는 것도 매우 자연스러운 일입니다. Redis 키는 문자열이므로 문자열 유형도 값으로 사용할 때 문자열을 다른 문자열에 매핑합니다. 문자열 데이터 형식은 HTML 조각 또는 페이지 캐싱과 같은 다양한 사용 사례에 유용합니다....","categories": ["cache"],
         "tags": ["redis","jupyter"],
